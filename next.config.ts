987758e4079159f255c6c4b8033d96dd2dbde5e6
Temp445 <acetemp345@gmail.com>
2025-07-19 15:22:44 +0530

    remove basepath

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,6 @@ import type { NextConfig } from 'next';
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
-  basePath: '/blog',
-  assetPrefix: '/blog/',
   trailingSlash: true,
 };
 
