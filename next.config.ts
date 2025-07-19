18fdc5fe11a6c2b1d5b1281ec86f2b8d375abe5e
Temp445 <acetemp345@gmail.com>
2025-07-19 14:41:47 +0530

    base path

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import {NextConfig} from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
- 
-const nextConfig: NextConfig = {};
- 
+import type { NextConfig } from 'next';
+
 const withNextIntl = createNextIntlPlugin();
+
+const nextConfig: NextConfig = {
+  basePath: '/blog',
+  assetPrefix: '/blog/',
+  trailingSlash: true,
+};
+
+// Wrap and export
 export default withNextIntl(nextConfig);
